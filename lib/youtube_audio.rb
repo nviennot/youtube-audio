ce86f87aa05b932b8e38c37bc626e1c94c009fb2
Tom Hosford <tom.hosford@gmail.com>
2013-06-01 02:14:57 -0400

    tweak

--- lib/youtube_audio.rb
+++ lib/youtube_audio.rb
@@ -11,7 +11,7 @@ class YoutubeAudio < Goliath::API
       #"sh -c \"curl -s '#{url}' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' | ffmpeg -i - -vn -f mp3 - \""
 
       #runs video crazy fast
-      #"sh -c \"curl -s '#{url}' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' \""
+      "sh -c \"curl -s '#{url}' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' \""
 
       #what is this stream data type anyway? (text/plain on youtube)
 
@@ -40,7 +40,7 @@ class YoutubeAudio < Goliath::API
     #shouldn't unbind only get called by the framework, not the user?
 
     #when the connection is closed, call unbind in the EM class
-    #env['decoder'].try(:unbind) rescue nil
+    env['decoder'].try(:unbind) rescue nil
   end
 
   def response(env)
@@ -49,37 +49,37 @@ class YoutubeAudio < Goliath::API
     @i ||= 0
     @i += 1
     puts "request #{@i}"
-    #puts "Fetching video=#{params['v']}"
-    #video_url = Url.new(params['v']).video_url
-    #puts "link: #{video_url}"
-    #puts AudioCoder.cmd(video_url)
+    puts "Fetching video=#{params['v']}"
+    video_url = Url.new(params['v']).video_url
+    puts "link: #{video_url}"
+    puts AudioCoder.cmd(video_url)
     #for some reason this causes several requests to be made
 
     #EventMachine.defer is used for integrating blocking operations into EventMachine's control flow.
     #http://futurechimp.org/2010/1/17/ruby-procs-and-eventmachine-callbacks
     #so why not use that here?
-    #decoder = EventMachine.popen(AudioCoder.cmd(video_url), AudioCoder, env)
+    decoder = EventMachine.popen(AudioCoder.cmd(video_url), AudioCoder, env)
 
-    pt = EM.add_periodic_timer(1) do
-      env.stream_send("test")
-    end
-
-    EM.add_timer(10) do
-      pt.cancel
-      env.stream_send("YEA!")
-      env.stream_close
-    end
-
-    #Doesn't this perform the same function ass 'on_close' above?
-    #env[ASYNC_CLOSE] = proc do
-      #unless env['closed']
-        #env['closed'] = true
-        #decoder.close_connection
-      #end
+    #pt = EM.add_periodic_timer(1) do
+      #env.stream_send("test")
     #end
 
-    [200, {}, Goliath::Response::STREAMING]
-    #[200, {'Content-Type' => 'audio/mp3'}, Goliath::Response::STREAMING]
+    #EM.add_timer(10) do
+      #pt.cancel
+      #env.stream_send("YEA!")
+      #env.stream_close
+    #end
+
+    #Doesn't this perform the same function ass 'on_close' above?
+    env[ASYNC_CLOSE] = proc do
+      unless env['closed']
+        env['closed'] = true
+        decoder.close_connection
+      end
+    end
+
+    #[200, {}, Goliath::Response::STREAMING]
+    [200, {'Content-Type' => 'audio/mp3'}, Goliath::Response::STREAMING]
   #rescue
     #[500, {}, 'we oopsed']
   end
